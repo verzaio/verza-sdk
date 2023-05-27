58208a48d7dc29c3e12ee4f0ac7b78b8702dee8c
zo0r <z@zo0r.me>
2023-05-27 00:30:15 -0400

    Update sdk

--- src/utils/misc.utils.ts
+++ src/utils/misc.utils.ts
@@ -9,6 +9,25 @@ export const isValidEnv = (): boolean => {
     console.debug(
       "Client Script cannot initiate from the same page, make sure the script is added/connected to the Server's settings",
     );
+
+    const el = document.createElement('div');
+    const h4 = document.createElement('h4');
+
+    el.append(h4);
+
+    el.style.position = 'absolute';
+    el.style.left = '0px';
+    el.style.top = '0px';
+    el.style.width = '100%';
+    el.style.display = 'flex';
+    el.style.margin = '0 auto';
+    el.style.padding = '30px';
+
+    h4.innerText =
+      "Verza Script: Client scripts cannot initiate from the same page, make sure the script is added and enabled in the Server's settings";
+
+    document.body.appendChild(el);
+
     return false;
   }
 
